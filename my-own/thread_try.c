6c09743b4ab9a08efe1c79be3438dacc9b7f23f2
Alessio Biancalana <dottorblaster@gmail.com>
2016-07-12 11:37:36 +0200

    better variable handling for for loops

--- my-own/thread_try.c
+++ my-own/thread_try.c
@@ -3,7 +3,8 @@
 #include <pthread.h>
 
 void *slave_fn1(void *v) {
-  for (size_t l = 0; l < 5; l++) {
+  size_t l;
+  for (l = 0; l < 5; l++) {
     printf("From the thread with love\n");
   }
   pthread_exit(NULL);
@@ -12,12 +13,13 @@ void *slave_fn1(void *v) {
 int main() {
   pthread_t tid1;
   pthread_t tid2;
+  size_t i;
   void *tret;
   printf("Creating thread\n");
   pthread_create(tid1, NULL, slave_fn1, NULL);
   printf("Thread created 1\n");
   printf("Thread created 2\n");
-  for (size_t i = 0; i < 5; i++) {
+  for (i = 0; i < 5; i++) {
     printf("Main!\n");
   }
   pthread_join(tid1, &tret);
