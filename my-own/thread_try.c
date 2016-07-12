305cb34628f6fd0e544d72388dadcb095021533a
Alessio Biancalana <dottorblaster@gmail.com>
2016-07-12 11:51:23 +0200

    finally joined threads

--- my-own/thread_try.c
+++ my-own/thread_try.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *slave_fn1(void *v) {
+void * slave_fn1() {
   size_t l;
   for (l = 0; l < 5; l++) {
     printf("From the thread with love\n");
@@ -12,15 +12,14 @@ void *slave_fn1(void *v) {
 
 int main() {
   pthread_t tid1;
-  pthread_t tid2;
   size_t i;
-  void *tret;
   printf("Creating thread\n");
-  pthread_create(tid1, NULL, slave_fn1, NULL);
+  pthread_create(&tid1, NULL, slave_fn1, NULL);
   printf("Thread created 1\n");
   printf("Thread created 2\n");
   for (i = 0; i < 5; i++) {
     printf("Main!\n");
   }
+  pthread_join(tid1, NULL);
   return 0;
 }
